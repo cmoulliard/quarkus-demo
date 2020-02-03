df5cf7b8af1d9379745d5bd28f37f2c8266b918c
Charles Moulliard <cmoulliard@redhat.com>
2020-02-03 10:17:04 +0100

    Fix: Review the test case to fix the error as the test was failing

--- src/test/java/dev/snowdrop/HelloApplicationTest.java
+++ src/test/java/dev/snowdrop/HelloApplicationTest.java
@@ -12,10 +12,10 @@ public class HelloApplicationTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/hello/polite/charles")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("Good afternoon,charles"));
     }
 
 }
